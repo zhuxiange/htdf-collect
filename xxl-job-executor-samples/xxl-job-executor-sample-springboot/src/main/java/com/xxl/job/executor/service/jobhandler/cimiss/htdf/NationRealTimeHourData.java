8d4c5cdc207591711824e0f99cb4121a86cd9761
ck <ck@192.168.1.8>
2017-12-12 11:54:58 +0800

    数据库连接池

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/cimiss/htdf/NationRealTimeHourData.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/cimiss/htdf/NationRealTimeHourData.java
@@ -8,11 +8,14 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.JobHander;
+import com.xxl.job.executor.db.DBConfig;
+import com.xxl.job.executor.db.DBInit;
 import com.xxl.job.executor.util.CimissConfig;
 import com.xxl.job.executor.util.CimissUtil;
 
@@ -28,13 +31,21 @@ public class NationRealTimeHourData extends IJobHandler {
 
 	@Autowired
 	private CimissConfig cimissConfig;
+	
+	@Autowired
+	private DBConfig dbConfig;
 
 	private static final Logger Logger = LoggerFactory.getLogger(NationRealTimeHourData.class);
 
 	@Override
 	public ReturnT<String> execute(String... params) throws Exception {
 
-		// 例子 -- 参数
+	 
+		
+		String sql = "";
+		List<Map<Object,Object>> list = dbConfig.select(sql, "1");
+		
+		/*// 例子 -- 参数
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("userId", cimissConfig.getUsername());
 		map.put("pwd", cimissConfig.getPassword());
@@ -52,7 +63,7 @@ public class NationRealTimeHourData extends IJobHandler {
 			for (String s : m) {
 				System.out.println(s + ":" + map1.get(s));
 			}
-		}
+		}*/
 		return new ReturnT<>(ReturnT.SUCCESS_CODE, "成功！");
 	}
 }
